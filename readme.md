b87af3a537bbf45025a530fc2d9344ded660676c
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2021-10-20 18:11:37 -0400

    +updates

---
+++ readme.md
@@ -0,0 +1,6 @@
+# CLI Rust
+
+This project is an aptemp for building a CLI with rust, 
+i'll try to make it a "script simplifier", and to learn rust in the process,
+
+## Last Update -> 20/10/2021
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,51 @@
-// Imports
-// use 
+// use std::env::{ args };
+// use std::path;
 
 
-fn test_calculate_two_number(one: i32, two:i32, fun:fn) -> i32{
-    return fun(one + two);
+fn test_calculate_two_number<F>(one: i32, two:i32, fun:F) -> i32 
+where F: Fn(i32) -> i32 {
+    fun(one + two)
 }
 
-struct Cli {
+// struct Cli {
 
-    pattern: String,
-    path: std::path::PathBuf,
-}
+//     pattern: String,
+//     path: std::path::PathBuf,
+// }
 
 
 fn main() {
 
     println!("Testing future CLI !!!");
     
-    println!("The result of one number plus anothers is : {}", test_calculate_two_number(10,11));
+    println!(
+        "The result of one number plus anothers is : {}", 
+        test_calculate_two_number(10,11, |one|{ one + 1 })
+    );
 
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    // let pattern = args().nth(1).expect("no pattern given");
+    // let path = args().nth(2).expect("no path given");
+    // let args = Cli {
+    //     pattern: pattern,
+    //     path: path::PathBuf::from(path),
+    // };
+    just_say_hello();
 
+}
+
+fn just_say_hello(){
+
+    println!("Hey there may dude!!");
+    get_primos(100);
+}
+
+
+
+fn get_primos(from:i32){
+    for n in 1.. from{
+        if n % 2 == 0 {
+            println!("{}",n);
+        }
+    }
 
 }
